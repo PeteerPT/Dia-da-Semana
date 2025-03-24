4db83626bd9598abde8a9172ad5b0d50e073718d
Pedro Oliveira <pedro_10pedro@hotmail.com>
2025-03-24 15:25:40 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+alert("Vamos lá! Informe qual é o dia da semana");
+let diaDaSemana = ["Sábado", "Domingo", "Sexta", "domingo", "Sabado", "sabado", "sábado"];
+
+
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 2.0; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela("h2", "Qual será o dia da semana?");
+}
+
+exibirMensagemInicial("h2", "Qual será o dia da semana?");
+
+
+let chute = prompt("Qual é o dia da semana?")
+
+if (chute == diaDaSemana) {
+    
+}
+
+if (diaDaSemana.includes(chute)) {
+    exibirTextoNaTela("h2", "Sextou, Sabadou e Domingou!!");
+} else {
+    exibirTextoNaTela("h2", "Bora trabaia...");
+}
+
